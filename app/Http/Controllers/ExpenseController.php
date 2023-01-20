6d413aae105236a8d04bd30d2619e812bac93fe5
DanFilipe2000 <anielilipe90@gmail.com>
2023-01-20 19:45:34 -0300

    fix[Controllers/ExpenseController.php]: unpay function

--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -89,7 +89,10 @@ class ExpenseController extends Controller
         }
 
         $attributes = $request->only([
-            'name'
+            'name',
+            'value',
+            'paid',
+            'due_date',
         ]);
 
         $expense->update($attributes);
@@ -130,7 +133,7 @@ class ExpenseController extends Controller
             abort(404);
         }
 
-        $expense->update(['paid' => 1]);
+        $expense->update(['paid' => 0]);
 
         return redirect('/dashboard')->with('success', 'Removido pagamento da despesa com sucesso');
     }
